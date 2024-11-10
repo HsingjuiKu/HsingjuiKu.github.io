bc70b6f000ebcceaa18b5c733a80bc5791befa50
HsingjuiKu <xingrui.gu@outlook.com>
2024-11-09 18:59:00 -0800

    Update 11.9

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
                         <div className="btn" onClick={() => { window.location.href = "https://github.com/HsingjuiKu" }}>
                             GITHUB
                         </div>
-                        <div className="btn" onClick={() => { window.location.href = "https://drive.google.com/file/d/1W6tw4YMhG_84DD-N6O8BCIsl0FdW228B/view?usp=sharing" }}>
+                        <div className="btn" onClick={() => { window.location.href = "https://drive.google.com/file/d/1serjpE8P0w5kweriYV7Uc7vJ50A-5R0z/view?usp=sharing" }}>
                             RESUME
                         </div>
                     </div>
